6ae0015e32c18b33a5fa7f11040af8ea2d0a83dd
Zakaria <alizaka@berkeley.edu>
2023-06-18 20:30:55 -0700

    HW6

--- scrabble.py
+++ scrabble.py
@@ -2,14 +2,21 @@ from wordscore import score_word
 
 def run_scrabble(rack):
 
+    valid_chars = "abcdefghijklmnopqrstuvwxyz*?"
+
     with open("sowpods.txt","r") as infile:
         raw_input = infile.readlines()
         data = [datum.strip('\n') for datum in raw_input]
 
-        # Error handling for empty rack
-        if len(rack) == 0:
-            return "You must input a minimum of 2 characters"
+        # Error handling for incorrect length rack
+        if len(rack) < 2 or len(rack) > 7:
+            return "You must input a minimum of 2 characters and a maximum of 7 characters."
 
-        # Error handling for invalid character
-        if not rack.isalpha() or '*' in rack or '?' in rack:
-            return "You input an invalid character. Only a-z, *, and ? are allowed."
+        # Error handling for invalid characters
+        for char in rack:
+            if char not in valid_chars:
+                return "You input an invalid character. Only a-z, *, and ? are allowed."
+
+        # Error handling for incorrect
+        if len(rack.strip('*?')) > 2:
+            return "You have too many wildcard characters."
