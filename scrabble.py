695f08715c542e459ba1d912e0cb52abe0a472d8
Zakaria <alizaka@berkeley.edu>
2023-06-18 20:23:12 -0700

    HW6

--- scrabble.py
+++ scrabble.py
@@ -6,6 +6,10 @@ def run_scrabble(rack):
         raw_input = infile.readlines()
         data = [datum.strip('\n') for datum in raw_input]
 
-        # Error handling
+        # Error handling for empty rack
         if len(rack) == 0:
             return "You must input a minimum of 2 characters"
+
+        # Error handling for invalid character
+        if not rack.isalpha() or '*' in rack or '?' in rack:
+            return "You input an invalid character. Only a-z, *, and ? are allowed."
